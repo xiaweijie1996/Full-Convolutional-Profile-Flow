be46c06f30ce43558a3d633277ab125ca4563240
xiaweijie1996 <xiaweijie1996@gmail.com>
2024-09-05 19:18:28 +0200

    s

--- exp/copula_ana/copula.ana.py
+++ exp/copula_ana/copula.ana.py
@@ -8,12 +8,16 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-from scipy.stats import t, gaussian_kde
 from scipy.interpolate import interp1d
 import yaml
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+
+font_path = 'tools/TIMES.TTF'
+font_prop = fm.FontProperties(fname=font_path)
+plt.rcParams['font.family'] = font_prop.get_name()
 
 from multicopula_model import EllipticalCopula
-from tools.evaluation_m import MMD_kernel, calculate_w_distances
 import alg.models_fcpflow_lin as fcpf
 import tools.tools_train as tl
 
@@ -39,7 +43,8 @@ print(pca_data.shape)
 # define the copula model and sample
 copula = EllipticalCopula(pca_data.T)
 copula.fit()
-samples = copula.sample(pca_data.shape[0])
+samples, inv_collection = copula.sample(pca_data.shape[0])
+samples = samples.T
 
 # define the fcpflow model and sample
 with open(os.path.join(_parent_path,'exp/copula_ana/FCPFlow/config.yaml')) as file:
@@ -51,17 +56,53 @@ model.load_state_dict(torch.load(os.path.join(_parent_path, 'exp/copula_ana/FCPF
 z = torch.randn(pca_data .shape[0], pca_data .shape[1])
 cond_test = torch.ones((pca_data.shape[0], 1))  
 gen_test = model.inverse(z, cond_test)
-re_data = torch.cat((gen_test, cond_test), dim=1)
-re_data = scaler.inverse_transform(re_data.cpu().detach().numpy())
+re_data = scaler.inverse_transform(gen_test.cpu().detach().numpy())
+
+# compute the empirical cdf
+cdf_pca_data_1, cdf_pca_data_2 = compute_empirical_marginal_cdf(pca_data)
+
+# evaluate the cdf of real data and samples
+cdf_real_1 = cdf_pca_data_1(pca_data[:, 0])
+cdf_real_2 = cdf_pca_data_2(pca_data[:, 1])
+cdf_sample_1 = cdf_pca_data_1(samples[:, 0])
+cdf_sample_2 = cdf_pca_data_2(samples[:, 1])
+cdf_re_1 = cdf_pca_data_1(re_data[:, 0])
+cdf_re_2 = cdf_pca_data_2(re_data[:, 1])
 
 # plot the data
-plt.figure()
-plt.scatter(pca_data[:,0], pca_data[:,1], label='Real data', alpha=0.3)
-plt.scatter(samples[0], samples[1], label='t-Copula', alpha=0.3)
-plt.scatter(re_data[:,0], re_data[:,1], label='FCPFlow', alpha=0.3)
-save_path = os.path.join(_parent_path, 'exp/copula_ana/copula_ana.png')
-plt.legend()
-plt.savefig(save_path)
+# plt.figure()
+# plt.scatter(pca_data[:,0], pca_data[:,1], label='Real data', alpha=0.3)
+# plt.scatter(samples[:,0], samples[:,1], label='t-Copula', alpha=0.3)
+# plt.scatter(re_data[:,0], re_data[:,1], label='FCPFlow', alpha=0.3)
+# save_path = os.path.join(_parent_path, 'exp/copula_ana/copula_ana.png')
+# plt.legend()
+# plt.savefig(save_path)
+# plt.close()
+
+# plot the cdf
+_size = 20
+plt.figure(figsize=(6, 6))
+plt.scatter(cdf_real_1, cdf_real_2, label='Real data', alpha=0.5)
+plt.scatter(cdf_sample_1, cdf_sample_2, label='t-Copula', alpha=0.2)
+plt.scatter(cdf_re_1, cdf_re_2, label='FCPFlow', alpha=0.5)
+plt.legend(fontsize=_size-6)
+# also apply this to legend's font
+for text in plt.gca().get_legend().get_texts():
+    text.set_fontproperties(font_prop)
+    text.set_fontsize(_size-6)
+# Apply the custom font properties to the x and y tick labels
+for label in plt.gca().get_xticklabels():
+    label.set_fontproperties(font_prop)  # Apply font properties to x-ticks
+    label.set_fontsize(_size-6)  # Ensure the size is applied
+
+for label in plt.gca().get_yticklabels():
+    label.set_fontproperties(font_prop)  # Apply font properties to y-ticks
+    label.set_fontsize(_size-6)  # Ensure the size is applied
+plt.xlabel('Cumulative Probability $u_1$ [-]', fontsize=_size, fontproperties=font_prop)
+plt.ylabel('Cumulative Probability $u_2$ [-]', fontsize=_size, fontproperties=font_prop)
+
+plt.savefig(os.path.join(_parent_path, 'exp/copula_ana/cdf_plot.png'))
 plt.close()
+
 
   
